999bc1276c0f827e22bb6fb51966ab9488afc4e3
Yashvir Gaming <64865096+YashvirGaming@users.noreply.github.com>
2025-08-08 21:07:38 +0400

    Update Launcher.bat

--- Launcher.bat
+++ Launcher.bat
@@ -4,12 +4,12 @@ cd /d "%~dp0"
 
 REM Prioritize EXE if exists, else use main.py
 if exist main.exe (
-    start "" main.exe
+    start "" videoexpress_checker.exe
 ) else (
     if exist main.py (
-        python main.py
+        python videoexpress_gui.py
     ) else (
-        echo main.py or main.exe not found in %CD%
+        echo videoexpress_gui.py or videoexpress_checker.exe not found in %CD%
         pause
     )
 )
